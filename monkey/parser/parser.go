f20e2809e98e5a4669ffbbd6b2f4e9dc056d18b3
esakat <esaka.tom@gmail.com>
2018-07-27 08:12:43 +0900

    エラー構文を追加した

--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"github.com/esakat/go_interpreter/monkey/ast"
 	"github.com/esakat/go_interpreter/monkey/lexer"
 	"github.com/esakat/go_interpreter/monkey/token"
@@ -11,10 +12,15 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+
+	errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// 2つのトークンを読み込み, curlTokenとpeekTokenの両方がセットされる
 	p.nextToken()
@@ -85,6 +91,17 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
